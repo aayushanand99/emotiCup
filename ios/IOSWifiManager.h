0caf66f8a40cc3ed53a9bf8841e23bbd74180706
aayush-anand <aayush-anand@MacBook-Pro.local>
2020-07-22 19:27:14 +0530

    gitignore

---
+++ ios/IOSWifiManager.h
@@ -0,0 +1,12 @@
+//
+//  IOSWifiManager.h
+//  emotiCup
+//
+//  Created by aayush-anand on 22/07/20.
+//
+
+#ifndef IOSWifiManager_h
+#define IOSWifiManager_h
+
+
+#endif /* IOSWifiManager_h */
---
+++ ios/IOSWifiManager.m
@@ -0,0 +1,8 @@
+//
+//  IOSWifiManager.m
+//  
+//
+//  Created by aayush-anand on 22/07/20.
+//
+
+#import <Foundation/Foundation.h>
